546274e7ebb46ba1f016e2ba91451ca4f53033e3
sudzekai <sudzekai@hotmail.com>
2026-09-17 18:58:07 +0300

    обновление абстракции IExecutor в соответствии с изменениями executor

--- executor.go
+++ executor.go
@@ -5,4 +5,5 @@ import "github.com/sudzekai-web-os/types"
 type IExecutor interface {
 	Execute(command string, args ...string) types.CommandResult
 	ExecuteInDirectory(directoryPath string, command string, args ...string) types.CommandResult
+	ExecuteWithInput(input, command string, args ...string) types.CommandResult
 }
